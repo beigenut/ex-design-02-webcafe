8739a3dc4d88c84644052208716b4cdbca18aa0b
Kim Hyemin <beigenut@gmail.com>
2018-04-02 17:26:56 +0900

    section 2 : inserting a video and controling w/ JS

--- js/webcafe.js
+++ js/webcafe.js
@@ -5,3 +5,27 @@ grid.addEventListener('click', function(){
     console.log('ok');
     container.classList.toggle('apha-grid');
 });
+
+// $ : jquery || doc : html {body} || ready : 문서 준비 객체 (func.)
+$(document).ready(function(){
+    var video = $('.news-video');
+
+    video.on('mouseover focusin', function(){
+        console.log('ok');
+        $(this).attr('autoplay','autoplay');
+    });
+    video.on('mouseout focusout', function(){
+        console.log('out');
+        $(this).attr('autoplay','false');
+    });
+}); 
+
+// native method 
+var video = document.querySelector('.news-video');
+
+video.addEventListener('mouseenter focusin', function(){
+    this.setAttribute('autoplay');
+});
+video.addEventListener('mouseleave focusout', function(){
+    this.removeAttribute('autoplay');
+});
